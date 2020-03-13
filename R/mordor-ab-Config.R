a08d68586d49d74a804f4677344b19e367bd6c96
Ben Arnold <bfarnold@gmail.com>
2020-03-13 07:30:10 -0700

    Add configuration file

---
+++ R/mordor-ab-Config.R
@@ -0,0 +1,45 @@
+
+#----------------------------
+# mordor-ab-Config.R
+#
+# Analysis of multiplex antibody 
+# endpoints in the MORDOR Niger trial
+#
+# Configuration file
+#----------------------------
+
+
+#----------------------------
+# load packages
+#----------------------------
+
+library(here)
+library(kableExtra)
+library(tidyverse)
+library(gridExtra)
+library(viridisLite)
+
+#----------------------------
+# stats packages
+#----------------------------
+library(mgcv)
+
+#----------------------------
+# set up for parallel computing
+#----------------------------
+library(foreach)
+library(doParallel)
+registerDoParallel(detectCores() - 1)
+
+
+#----------------------------
+# custom color pallettes
+#----------------------------
+
+# safe color blind palette
+# http://jfly.iam.u-tokyo.ac.jp/color/
+# http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
+cbpal <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# Viridis color palette
+vircols <- viridis(9,begin=0,end=0.97)
