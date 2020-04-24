06d97ad0f505dda744935638b6d2351c45a5f78d
Ben Arnold <bfarnold@gmail.com>
2020-04-23 17:45:55 -0700

    Small changes to plot aesthetics for MFI differences between arms

--- R/mordor-ab-Config.R
+++ R/mordor-ab-Config.R
@@ -44,9 +44,12 @@ registerDoParallel(detectCores() - 1)
 # Reference: Bang Wong, Nature Methods 2011: https://www.nature.com/articles/nmeth.1618
 cbpal <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-# NY Times rainbow from the Upshot
+# NY Times rainbow palettes from the Upshot
 # https://www.nytimes.com/interactive/2020/03/21/upshot/coronavirus-deaths-by-country.html
 nytpal <- c("#510000", "#AC112D", "#EC6D47", "#F2A058", "#F7D269", "#839772", "#325D8A")
+
+# https://www.nytimes.com/2020/04/23/us/coronavirus-early-outbreaks-cities.html
+nytpal2 <- c("#7486AD", "#77658E","#A1493B","#C17B3F", "#E6CA83")
 
 # Viridis color palette
 vircols <- viridis(9,begin=0,end=0.97)
