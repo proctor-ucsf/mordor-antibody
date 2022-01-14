adcdab623faa09e84e439bfc29d914433d43d6c8
Ben Arnold <bfarnold@gmail.com>
2022-01-14 14:40:09 -0800

    Update title. Output figures to pdf (vector) format. Make figure axes sentence case.

--- R/mordor-ab-run-all.R
+++ R/mordor-ab-run-all.R
@@ -49,21 +49,21 @@ rmarkdown::render(here::here("R/04-mordor-ab-baseline-balance.Rmd"),
                   output_file = here::here("R/04-mordor-ab-baseline-balance.html"))
 
 #-----------------------------------
-# SI Figures 1 and 2
+# SI Figures 3 and 8
 # descriptive summaries
 #-----------------------------------
 rmarkdown::render(here::here("R/05-mordor-ab-descriptive-summary.Rmd"),
                   output_file = here::here("R/05-mordor-ab-descriptive-summary.html"))
 
 #-----------------------------------
-# SI Figure 3
+# SI Figure 1
 # measurement timing
 #-----------------------------------
 rmarkdown::render(here::here("R/06-mordor-ab-measurement-timing.Rmd"),
                   output_file = here::here("R/06-mordor-ab-measurement-timing.html"))
 
 #-----------------------------------
-# SI Figure 4
+# SI Figure 2
 # pairwise correlation of IgG responses
 #-----------------------------------
 rmarkdown::render(here::here("R/07-mordor-ab-pairwise-ab-correlation.Rmd"),
@@ -71,18 +71,25 @@ rmarkdown::render(here::here("R/07-mordor-ab-pairwise-ab-correlation.Rmd"),
 
 #-----------------------------------
 # Community level means
+# Fig 2a, 3a
+# Supp Figs 5, 10
+# Supp Table 3
 #-----------------------------------
 rmarkdown::render(here::here("R/08-mordor-ab-community-means.Rmd"),
                   output_file = here::here("R/08-mordor-ab-community-means.html"))
 
 #-----------------------------------
 # Age dependent antibody curves
+# Fig 3b
+# Supp Figs 4, 9
 #-----------------------------------
 rmarkdown::render(here::here("R/09-mordor-ab-age-curves.Rmd"),
                   output_file = here::here("R/09-mordor-ab-age-curves.html"))
 
 #-----------------------------------
 # Primary analysis
+# Fig 2b, 2c, 2d
+# Fig 3c, 3d
 #-----------------------------------
 rmarkdown::render(here::here("R/10-mordor-ab-mean-diffs.Rmd"),
                   output_file = here::here("R/10-mordor-ab-mean-diffs.html"))
@@ -92,12 +99,14 @@ rmarkdown::render(here::here("R/11-mordor-ab-malaria-composite.Rmd"),
 
 #-----------------------------------
 # Longitudinal analyses
+# Supp Tables 1, 2
 #-----------------------------------
 rmarkdown::render(here::here("R/12-mordor-ab-longitudinal.Rmd"),
                   output_file = here::here("R/12-mordor-ab-longitudinal.html"))
 
 #-----------------------------------
 # Pre-specified Subgroup analyses
+# Supp Figs 11, 12
 #-----------------------------------
 rmarkdown::render(here::here("R/13-mordor-ab-subgroup-analyses.Rmd"),
                   output_file = here::here("R/13-mordor-ab-subgroup-analyses.html"))
@@ -105,6 +114,7 @@ rmarkdown::render(here::here("R/13-mordor-ab-subgroup-analyses.Rmd"),
 #-----------------------------------
 # Sensitivity analysis that varies
 # MFI cutoff values by +/- 20%
+# Supp Fig 14
 #-----------------------------------
 rmarkdown::render(here::here("R/14-mordor-ab-cutoff-sensitivity-analysis.Rmd"),
                   output_file = here::here("R/14-mordor-ab-cutoff-sensitivity-analysis.html"))
@@ -114,6 +124,7 @@ rmarkdown::render(here::here("R/14-mordor-ab-cutoff-sensitivity-analysis.Rmd"),
 # a leave-one-out (LOO) analysis
 # and computes jackknife estimates
 # of the bias
+# Supp Fig 15
 #-----------------------------------
 rmarkdown::render(here::here("R/15-mordor-ab-loo-sensitivity-analysis.Rmd"),
                   output_file = here::here("R/15-mordor-ab-loo-sensitivity-analysis.html"))
@@ -124,6 +135,7 @@ rmarkdown::render(here::here("R/15-mordor-ab-loo-sensitivity-analysis.Rmd"),
 # and (not-prespecified) subgroup
 # analysis by baseline malaria
 # parasitemia
+# Supp Fig 13
 #-----------------------------------
 rmarkdown::render(here::here("R/16-mordor-ab-pf-infection-comparison.Rmd"),
                   output_file = here::here("R/16-mordor-ab-pf-infection-comparison.html"))
